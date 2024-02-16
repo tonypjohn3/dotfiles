55c90238b40b66f45cf8dc61f57c432b42f870ee
Tony John <tony.john@cognizant.com>
2024-02-16 13:10:37 +1100

    update buffer navigation keymap; move all colorscheme plugins to single file

---
+++ nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,49 @@
+return {
+  { "Shatur/neovim-ayu" },
+  { "catppuccin/nvim", name = "catppuccin", },
+  {
+    'projekt0n/github-nvim-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function ()
+      require("github-theme").setup()
+      vim.cmd("colorscheme github_dark_default")
+    end
+  },
+  { "rebelot/kanagawa.nvim" },
+  { "dasupradyumna/midnight.nvim" },
+  { "miikanissi/modus-themes.nvim" },
+  { "bluz71/vim-moonfly-colors", name = "moonfly", },
+  { "oxfist/night-owl.nvim" },
+  { "bluz71/vim-nightfly-colors", name = "nightfly", },
+  { "EdenEast/nightfox.nvim" },
+  { "nordtheme/vim", name = "nord", },
+  { "AlexvZyl/nordic.nvim" },
+  {
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    -- priority = 1000,
+    -- lazy = false,
+    config = function()
+      require('onedark').setup {
+        -- Set a style preset. 'dark' is default.
+        -- style = 'darker', -- dark, darker, cool, deep, warm, warmer, light
+      }
+      -- require('onedark').load()
+    end,
+  },
+  { "rose-pine/neovim", name = "rose-pine", },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function ()
+      require("tokyonight").setup({
+        style = "night",
+      })
+      -- require("tokyonight").load()
+    end,
+  },
+}
+
