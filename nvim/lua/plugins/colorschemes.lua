a814e2d75adb2a9a107a58f2b2b6183748ab4783
Tony John <tony.john@cognizant.com>
2024-02-20 16:41:09 +1100

    LSP keymap change, colorscheme change
    * Colorscheme default changed to nightfly
    * LSP keymap for signature hover doc changed to Ctrl-s from Ctrl-k (fixing Ctrl-k tmux navigation)

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,44 +1,52 @@
 return {
   { "Shatur/neovim-ayu" },
-  { "catppuccin/nvim", name = "catppuccin", },
+  { "catppuccin/nvim",  name = "catppuccin" },
   {
-    'projekt0n/github-nvim-theme',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    "projekt0n/github-nvim-theme",
+    lazy = false,  -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function ()
+    config = function()
       require("github-theme").setup()
-      vim.cmd("colorscheme github_dark_default")
-    end
+      -- vim.cmd("colorscheme github_dark_default")
+    end,
   },
   { "rebelot/kanagawa.nvim" },
+  { "marko-cerovac/material.nvim" },
   { "dasupradyumna/midnight.nvim" },
   { "miikanissi/modus-themes.nvim" },
-  { "bluz71/vim-moonfly-colors", name = "moonfly", },
+  { "bluz71/vim-moonfly-colors",   name = "moonfly" },
   { "oxfist/night-owl.nvim" },
-  { "bluz71/vim-nightfly-colors", name = "nightfly", },
+  {
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
+    lazy = false,
+    config = function()
+      vim.cmd("colorscheme nightfly")
+    end,
+  },
   { "EdenEast/nightfox.nvim" },
-  { "nordtheme/vim", name = "nord", },
+  { "nordtheme/vim",         name = "nord" },
   { "AlexvZyl/nordic.nvim" },
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    "navarasu/onedark.nvim",
     -- priority = 1000,
     -- lazy = false,
     config = function()
-      require('onedark').setup {
+      require("onedark").setup({
         -- Set a style preset. 'dark' is default.
         -- style = 'darker', -- dark, darker, cool, deep, warm, warmer, light
-      }
+      })
       -- require('onedark').load()
     end,
   },
-  { "rose-pine/neovim", name = "rose-pine", },
+  { "rose-pine/neovim", name = "rose-pine" },
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
-    config = function ()
+    config = function()
       require("tokyonight").setup({
         style = "night",
       })
@@ -46,4 +54,3 @@ return {
     end,
   },
 }
-
