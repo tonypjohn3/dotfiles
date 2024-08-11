8d46f8e6e02501232acaf5ed0b50599f8498a2c6
Tony John <tony.john@cognizant.com>
2024-08-11 18:10:35 +1000

    change neo tree reveal to neotree toggle

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -17,7 +17,7 @@ return {
     },
   },
   config = function()
-    vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", {})
+    vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", {})
     vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
   end,
 }
